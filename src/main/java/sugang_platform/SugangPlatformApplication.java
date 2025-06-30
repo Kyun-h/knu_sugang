713af3edb4d75b2bfc06a27bddd18e74bae5729b
Kyun-h <qwertyuiopassdf48@gmail.com>
2025-06-30 10:59:40 +0900

    test

---
+++ src/main/java/sugang_platform/SugangPlatformApplication.java
@@ -0,0 +1,13 @@
+package sugang_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SugangPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SugangPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knu/sugang/platform/SugangPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.sugang.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SugangPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
