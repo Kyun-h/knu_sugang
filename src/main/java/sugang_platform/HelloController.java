ff201c50bbf4a7cbd65b5d59e83a97ab1ae1d40e
Kyun-h <qwertyuiopassdf48@gmail.com>
2025-06-30 17:05:01 +0900

    fix

---
+++ src/main/java/sugang_platform/HelloController.java
@@ -0,0 +1,14 @@
+package sugang_platform;
+
+import jakarta.persistence.Id;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String hello() {
+    return "Hello from Docker!";
+  }
+}
